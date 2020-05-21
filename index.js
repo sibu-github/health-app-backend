530761b5b2990d0576a091936116f4bccfd869a6
Sibaprasad Maiti <sibaprasad.maiti@varatechnology.com>
2020-05-21 11:43:40 +0530

    message modified

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Node.js Sample!\n');
+  res.send('Hello Node.js Sample!\n Yay!!!! CI&CD working!!!');
 });
 
 let port = process.env.PORT || PORT;
