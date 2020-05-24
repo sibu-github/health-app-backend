318eaf06fdf73e88c774c2a15900d1636580f337
siva <sivateja1034@gmail.com>
2020-05-24 13:19:15 +0530

    version3.1

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/*created for the testing of the module */
-router.get('/api/', function(req, res, next) {
-    res.send('hello world');
-});
-
-module.exports = router;
