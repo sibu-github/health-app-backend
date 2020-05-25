c766d87dc107da33c7129fd4ceeed238cd479b5e
siva <sivateja1034@gmail.com>
2020-05-25 23:00:59 +0530

    version6.1

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path');
 const userRouter = require('./routers/userResponseRoutes');
 const locationRouter = require('./routers/locationRoutes');
 const userinfoRouter = require('./routers/userInfoRoutes');
