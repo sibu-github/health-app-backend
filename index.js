2cf1ceb8f1d5b75836087dfdb5b8d5ac0edae9b2
Sibaprasad Maiti <sibaprasad.maiti@varatechnology.com>
2020-05-26 08:42:46 +0530

    eslint added

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const app = require('./app.js');
 require('./db/mongoose');
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log('Server is UP! on port ' + port)
-})
+  console.log('Server is UP! on port ' + port);
+});
