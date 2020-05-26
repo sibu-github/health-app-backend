3675ce7a92e5b459bc6bd82ffb978fb40c173163
Sibaprasad Maiti <sibaprasad.maiti@varatechnology.com>
2020-05-26 18:56:42 +0530

    /landpage added in app.js

--- app.js
+++ app.js
@@ -7,26 +7,37 @@ const locationRouter = require('./routers/locationRoutes');
 const userinfoRouter = require('./routers/userInfoRoutes');
 const apilogRouter = require('./routers/apilogRoutes');
 const questionRouter = require('./routers/questionsRoutes');
-const adminRouter = require("./routers/hradminRoutes");
+const adminRouter = require('./routers/hradminRoutes');
 const DIST_FOLDER = './dist';
 const INDEX_FILE_NAME = 'index.html';
 
-const app = express()
-app.use(express.json())
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: false }))
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use((req, res, next) => {
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept");
-        res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE,OPTIONS");
-        next();
-    })
-    // for serving static files
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With,Content-Type,Accept'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,POST,PATCH,DELETE,OPTIONS'
+  );
+  next();
+});
+// for serving static files
 app.use(express.static(path.join(__dirname, DIST_FOLDER)));
 
 // for serving the angular code,
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, DIST_FOLDER, INDEX_FILE_NAME));
+  res.sendFile(path.join(__dirname, DIST_FOLDER, INDEX_FILE_NAME));
+});
+
+// for serving the angular code,
+app.get('/landpage', (req, res) => {
+  res.sendFile(path.join(__dirname, DIST_FOLDER, INDEX_FILE_NAME));
 });
 
 app.use(userRouter);
@@ -36,9 +47,7 @@ app.use(apilogRouter);
 app.use(questionRouter);
 app.use(adminRouter);
 app.get('/login', (req, res) => {
-    res.send('Hello World')
-
-})
-
+  res.send('Hello World');
+});
 
 module.exports = app;
