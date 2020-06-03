50724fe16b10f8e2ba3a74a78569202c815a9b8b
Sibaprasad Maiti <sibu.it13@gmail.com>
2020-06-03 22:28:35 +0530

    unnecessary logs removed

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-console.log('Mongo DB URL:', process.env.MONGODB_URL);
 mongoose.connect(process.env.MONGODB_URL, {
   useCreateIndex: true,
   useFindAndModify: false,
--- routers/locationRoutes.js
+++ routers/locationRoutes.js
@@ -18,19 +18,8 @@ router.post('/api/postlocation', auth, async (req, res) => {
 });
 
 router.get('/api/getlocation/', auth, async (req, res) => {
-  console.log('in the location router');
-
-  console.log('MONGODB_URL', process.env.MONGODB_URL);
-  console.log('CLIENT_ID', process.env.CLIENT_ID);
-  console.log('CLIENT_SECRET', process.env.CLIENT_SECRET);
-  console.log('TENANT_ID', process.env.TENANT_ID);
-  console.log('REDIRECT_URL', process.env.REDIRECT_URL);
-  console.log('JWT_SECRET_KEY', process.env.JWT_SECRET_KEY);
-
   try {
-    console.log('before query');
     const location = await Location.find({ active: true });
-    console.log('after query');
     res.status(200).json(location);
   } catch (err) {
     //added status
