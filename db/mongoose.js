14d848506558a4c4fe84f6a027d8f69089f3ae6e
Sibaprasad Maiti <sibu.it13@gmail.com>
2020-06-03 22:12:10 +0530

    log added

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+console.log('Mongo DB URL:', process.env.MONGODB_URL);
 mongoose.connect(process.env.MONGODB_URL, {
   useCreateIndex: true,
   useFindAndModify: false,
--- routers/locationRoutes.js
+++ routers/locationRoutes.js
@@ -1,30 +1,34 @@
-const mongoose = require("mongoose");
-const express = require("express");
+const mongoose = require('mongoose');
+const express = require('express');
 const router = new express.Router();
-const auth = require("../middleware/auth");
+const auth = require('../middleware/auth');
 
-const Location = require("../models/locationList");
+const Location = require('../models/locationList');
 
-router.post("/api/postlocation", auth, async(req, res) => {
-    try {
-        const location = new Location(req.body);
-        await location.save();
-        //added the json and status
-        res.status(200).json(location);
-    } catch (err) {
-        //added status 500 and message in json
-        res.status(500).json({ message: "enter the proper details" });
-    }
+router.post('/api/postlocation', auth, async (req, res) => {
+  try {
+    const location = new Location(req.body);
+    await location.save();
+    //added the json and status
+    res.status(200).json(location);
+  } catch (err) {
+    //added status 500 and message in json
+    res.status(500).json({ message: 'enter the proper details' });
+  }
 });
 
-router.get("/api/getlocation/", auth, async(req, res) => {
-    try {
-        const location = await Location.find({ active: true });
-        res.status(200).json(location);
-    } catch (err) {
-        //added status 
-        res.status(500).json({ message: "location is not found" });
-    }
+router.get('/api/getlocation/', auth, async (req, res) => {
+  console.log('in the location router');
+
+  try {
+    console.log('before query');
+    const location = await Location.find({ active: true });
+    console.log('after query');
+    res.status(200).json(location);
+  } catch (err) {
+    //added status
+    res.status(500).json({ message: 'location is not found' });
+  }
 });
 
 module.exports = router;
