335ff9956388922da1e7ebc121e6a1846bbd3124
Sibaprasad Maiti <sibu.it13@gmail.com>
2020-06-14 01:09:28 +0530

    dotenv reference removed from app.js

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const apilogRouter = require('./routers/apilogRoutes');
 const questionRouter = require('./routers/questionsRoutes');
 const adminRouter = require('./routers/hradminRoutes');
 const tokenRouter = require('./routers/tokenRoutes');
-require('dotenv').config({ path: 'ENV_FILENAME' });
 //jwt JwtTokenRouter
 const JwtTokenRouter = require('./routers/jwtAuthRoutes');
 const morgan = require('morgan');
